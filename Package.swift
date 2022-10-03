a457dc3e60420c295988b846e7af1486302e3185
Magdy zamel <Magdy@dapi.com>
2022-10-03 18:24:42 +0400

    v.1.0.0-beta5

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DapiSDK",
-            url: "https://github.com/dapi-co/Dapi-iOS/releases/download/1.0.0-beta4/DapiSDK.zip",
-            checksum: "367c4d5def852b1600d6e8a9ff45ea2709bcee936cf7c0e72f6513add8ae7bd4")
+            url: "https://github.com/dapi-co/Dapi-iOS/releases/download/1.0.0-beta5/DapiSDK.zip",
+            checksum: "c31d6846056960eda3c6e3e0e819cf2700daf118871d1333e9643c74b79a6aa7")
     ]
 )
